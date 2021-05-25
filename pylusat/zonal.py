3a72a133794e11ebcf3485f5b5e6adc30eae1f16
chjch <chj.chen@hotmail.com>
2021-05-24 23:00:08 -0400

    update zonal_stats_raster() to use RasterManager

--- pylusat/zonal.py
+++ pylusat/zonal.py
@@ -3,7 +3,8 @@ import pandas as pd
 from affine import Affine
 from geopandas import GeoDataFrame
 from pylusat.base import GeoDataFrameManager
-from pylusat.utils import read_raster
+from pylusat.base import RasterManager
+from osgeo import osr
 
 
 def _to_affine(cellsize, max_y, min_x):
@@ -12,9 +13,13 @@ def _to_affine(cellsize, max_y, min_x):
 
 
 def zonal_stats_raster(zone_gdf, raster, stats=None,
-                       stats_prefix='zone', nodata=None):
+                       stats_prefix='zonal', nodata=None):
     """
-    Return specified stats for each geometry in the zone GeoDataFrame.
+    Calculate specified stats for each geometry in the zone GeoDataFrame.
+
+    If raster and zone GeoDataFrame were in different spatial reference
+    systems, the raster will be warped (reprojected) to match the spatial
+    reference of the zone GeoDataFrame.
 
     Parameters
     ----------
@@ -30,10 +35,10 @@ def zonal_stats_raster(zone_gdf, raster, stats=None,
         Other valid stats are ['sum', 'std', 'median', 'majority', 'minority',
         'unique', 'range', 'nodata', 'nan'].
         See rasterstats for more details.
-    stats_prefix : str, default 'zone'
+    stats_prefix : str, default 'zonal'
         The prefix used to name the output columns of the calculated stats.
         The output column name will be concatenated by a underscore.
-        e.g., 'zone_mean'
+        e.g., 'zonal_mean'
     nodata : int or float
         Value for no data cells.
 
@@ -45,18 +50,29 @@ def zonal_stats_raster(zone_gdf, raster, stats=None,
     """
     if not GeoDataFrameManager(zone_gdf).geom_type_validate("Polygon"):
         raise ValueError("zone GeoDataFrame must be polygon.")
+    gdf_srs = osr.SpatialReference(wkt=zone_gdf.crs.to_wkt())
 
-    raster_arr, cellsize, max_y, min_x, nodata = read_raster(raster, nodata)
+    rast_manager = RasterManager(raster, nodata)
+    rast_srs = rast_manager.srs
+    if not rast_srs.IsSame(gdf_srs):
+        rast_ds = rast_manager.reproject(srs=gdf_srs)
+        rast_arr, cellsize, max_y, min_x, nodata = RasterManager.as_array(
+            rast_ds, nodata
+        )
+    else:
+        rast_arr, cellsize, max_y, min_x, nodata = rast_manager.to_array()
+
     affine = _to_affine(cellsize, max_y, min_x)
-    zonal_output = zonal_stats(vectors=zone_gdf.geometry, raster=raster_arr,
+    zonal_output = zonal_stats(vectors=zone_gdf.geometry, raster=rast_arr,
                                nodata=nodata, affine=affine, stats=stats,
                                all_touched=True)
     zone_gdf.reset_index(drop=True, inplace=True)
     if not stats:
         from rasterstats.utils import DEFAULT_STATS
         stats = DEFAULT_STATS
-    else:
+    elif isinstance(stats, str):
         stats = stats.split()
+
     col_names = [f'{stats_prefix}_{stat}' for stat in stats]
     output_gdf = zone_gdf.join(pd.DataFrame(zonal_output))
     output_gdf.rename(columns=dict(zip(stats, col_names)), inplace=True)
