c597bd159671d62305187965cb4ef81c67541420
chjch <chj.chen@hotmail.com>
2021-05-26 10:56:28 -0400

    change test from unittest to pytest

--- pylusat/tests/test_geotools.py
+++ pylusat/tests/test_geotools.py
@@ -1,29 +1,25 @@
-import os
 import geopandas as gpd
 from pylusat.geotools import spatial_join, select_by_location
-import unittest
+from pylusat.datasets import get_path
+import pytest
 
 
-class TestGeoTools(unittest.TestCase):
-
-    schools = "schools/schools.shp"     # point geometry
-    acs2016 = "acs2016/acs2016.shp"     # polygon geometry
-    dataset_path = os.path.join(os.path.dirname(os.getcwd()), "datasets")
-
-    schools_shp = os.path.join(dataset_path, schools)
-    acs2016_shp = os.path.join(dataset_path, acs2016)
-
-    @classmethod
-    def setUpClass(cls):
-        cls.schools_gdf = gpd.read_file(cls.schools_shp)
-        cls.acs2016_gdf = gpd.read_file(cls.acs2016_shp)
-
-    def test_spatial_join(self):
-        print(spatial_join(self.acs2016_gdf, self.schools_gdf))
-
-    def test_select_by_location(self):
-        select_by_location(self.acs2016_gdf, self.schools_gdf)
+@pytest.fixture
+def acs2016_gdf():
+    return gpd.read_file(get_path("acs2016"))
 
 
-if __name__ == "__main__":
-    unittest.main()
+@pytest.fixture
+def schools_gdf():
+    return gpd.read_file(get_path("schools"))
+
+
+def test_spatial_join(acs2016_gdf, schools_gdf):
+    result = spatial_join(acs2016_gdf, schools_gdf,
+                          cols_agg={'ENROLLMENT': ['sum']})
+    assert result.iloc[2, -1] == 345
+
+
+def test_select_by_location(acs2016_gdf, schools_gdf):
+    result = select_by_location(acs2016_gdf, schools_gdf)
+    assert len(result) == 75
