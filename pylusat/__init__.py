fb710637cd75e855f9bd880bd29270211fd0ec1b
chjch <chj.chen@hotmail.com>
2021-05-27 00:35:10 -0400

    delete import geopandas

--- pylusat/__init__.py
+++ pylusat/__init__.py
@@ -1,9 +1,1 @@
-from pylusat.geotools import (select_by_location, erase,
-                              within_dist, spatial_join)
-from pylusat.base import GeoDataFrameManager
-
-import geopandas as gpd
-import pandas as pd
-import numpy as np
-
 from pylusat._version import __version__
