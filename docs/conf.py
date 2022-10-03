aab4912b721a2648b8dbba3bf14f7dd82ea9bdc5
chjch <chj.chen@hotmail.com>
2022-10-02 22:36:08 -0400

    Add intersphinx

--- docs/conf.py
+++ docs/conf.py
@@ -14,8 +14,8 @@ import os
 import sys
 import sphinx_rtd_theme
 
-sys.path.insert(0, os.path.abspath(".."))
-sys.path.insert(0, os.path.abspath('../../'))
+sys.path.insert(0, os.path.abspath(""))
+sys.path.insert(0, os.path.abspath('../'))
 
 # -- Project information -----------------------------------------------------
 
@@ -32,17 +32,35 @@ author = 'The GALUP Team'
 extensions = [
     'sphinx_rtd_theme',
     'sphinx.ext.napoleon',
-    'sphinx.ext.autodoc'
+    'sphinx.ext.autodoc',
+    'sphinx.ext.intersphinx'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
+# The suffix(es) of source filenames.
+# You can specify multiple suffix as a list of string:
+# source_suffix = ['.rst', '.md']
+source_suffix = '.rst'
+
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = []
+exclude_patterns = ['_build']
 
+# The master toctree document.
+master_doc = 'index'
+
+# If true, '()' will be appended to :func: etc. cross-reference text.
+add_function_parentheses = True
+
+# If true, the current module name will be prepended to all description
+# unit titles (such as .. function::).
+add_module_names = True
+
+# The name of the Pygments (syntax highlighting) style to use.
+pygments_style = 'sphinx'
 
 # -- Options for HTML output -------------------------------------------------
 
@@ -55,3 +73,14 @@ html_theme = "sphinx_rtd_theme"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+
+# Example configuration for intersphinx: refer to the Python standard library.
+intersphinx_mapping = {
+    "python": ("https://docs.python.org/", None),
+    "gdal": ("https://gdal.org/", None),
+    "numpy": ("https://numpy.org/doc/stable", None),
+    "pandas": ("https://pandas.pydata.org/docs/reference/", None),
+    "geopandas": ("https://geopandas.org/en/stable/", None),
+    "rasterio": ("https://rasterio.readthedocs.io/en/latest/", None),
+}
